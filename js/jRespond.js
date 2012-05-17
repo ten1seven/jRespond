182c81bb6bb7d1f71155914674aedd79bb408f4a
Jeremy Fields <jeremy.fields@viget.com>
2012-05-17 14:07:36 -0600

    Updated description

--- js/jRespond.js
+++ js/jRespond.js
@@ -1,7 +1,7 @@
 /*
 	jRespond holds a list of user-defined functions that are either fired or destroyed
 	based on media breakpoint. Because jRespond was built to be browser agnostic, it does NOT
-	sniff for media queries in the CSS. All media breakpoints need to be defined below.
+	sniff for media queries in the CSS. All media breakpoints need to be defined manually.
 */
 jRespond = (function(window,document,undefined) {
 	
