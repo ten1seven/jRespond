6c855db467f27d5f49a288806123e3882afb1bd3
Jeremy Fields <jeremy.fields@viget.com>
2012-05-19 16:17:34 -0600

    Adding license information

--- js/jRespond.js
+++ js/jRespond.js
@@ -1,7 +1,8 @@
 /*
-	jRespond holds a list of user-defined functions that are either fired or destroyed
-	based on media breakpoint. Because jRespond was built to be browser agnostic, it does NOT
-	sniff for media queries in the CSS. All media breakpoints need to be defined manually.
+	jRespond.js (a simple way to globally manage javascript on responsive websites)
+	version 0.7
+	(c) 2012 Jeremy Fields [jeremy.fields@viget.com]
+	released under the MIT license
 */
 (function(win,doc,undefine) {
 	
