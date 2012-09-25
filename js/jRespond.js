4e071b5076c0dd9445c69f228cfb6b22a4011b1e
Jeremy Fields <jeremy.fields@viget.com>
2012-09-25 12:00:43 -0600

    Updated min version

--- js/jRespond.js
+++ js/jRespond.js
@@ -1,6 +1,6 @@
 /*
  * jRespond.js (a simple way to globally manage javascript on responsive websites)
- * version 0.8.1
+ * version 0.8.2
  * (c) 2012 Jeremy Fields [jeremy.fields@viget.com]
  * released under the MIT license
  */
